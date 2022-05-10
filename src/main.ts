89c45d51c02ba04cdcb02a638fd0fbd2b1f7c6f8
Ignacio Rodriguez Villasuso <ignaciorodvil@gmail.com>
2022-05-10 19:14:09 -0300

    Heroku fixes

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const cors = require('cors')
+  app.use(cors({ 
+    origin: "https://securitize-challenge.herokuapp.com/", 
+    credentials: true 
+   }))
   await app.listen( process.env.PORT || 3000);
 }
 bootstrap();
