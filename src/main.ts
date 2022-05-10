26826d600cfd707eac8040ed975a8930904e4734
Ignacio Rodriguez Villasuso <ignaciorodvil@gmail.com>
2022-05-10 19:33:32 -0300

    Heroku fixes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-import { NestFactory, FastifyAdapter } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new FastifyAdapter(), { cors: true });
-  const cors = require('cors')
-  app.use(cors({ 
-    origin: "https://securitize-challenge.herokuapp.com/", 
-    credentials: true 
-   }))
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen( process.env.PORT || 3000);
 }
 bootstrap();
